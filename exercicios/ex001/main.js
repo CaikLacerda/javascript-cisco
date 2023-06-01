41457c6eae79fe55cdeaff19b039693a9aa256ca
Caik <caiklacerda9@gmail.com>
2023-06-01 18:05:55 -0300

    primeiro ex de javascript

---
+++ exercicios/ex001/main.js
@@ -0,0 +1,15 @@
+console.log("Caik Dias Lacerda");
+
+console.log("2004");
+
+console.log(2004);
+
+console.log("Caik", "Lacerda", "2004");
+
+console.log("Caik");
+console.log("Lacerda");
+console.log("2004");
+
+console.log("Caik" + "Lacerda" + "2004");
+
+console.log("Caik " + "Lacerda " + "2004")
