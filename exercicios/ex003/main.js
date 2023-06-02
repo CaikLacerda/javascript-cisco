c21984b9e8f035262fa3870903945177788ebaa7
Caik <caiklacerda9@gmail.com>
2023-06-02 17:46:08 -0300

    ex003 criado

---
+++ exercicios/ex003/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function greeting() {
+    console.log("Hello");
+    console.log("World");
+}
+
+console.log("let´s begin");
+greeting();
+console.log("and again");
+greeting();
+console.log("and once more");
+greeting();
